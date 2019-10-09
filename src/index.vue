86e49b4300d5730acfb55eb486ab2329b316362d
rainboy <rainboylvx@qq.com>
2019-10-10 00:06:42 +0800

    [加入] vue-router

---
+++ src/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>导航</h1>
+    <ul>
+      <li> 
+        <router-link to="/foo">Go to Foo</router-link>
+      </li>
+      <li>
+        <router-link to="/bar">Go to Bar</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueRouter from 'vue-router'
+import index from './index.vue'
+Vue.use(vueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+  {
+    path:'/',component:index
+  }
+]
+
+const router = new vueRouter({routes})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
